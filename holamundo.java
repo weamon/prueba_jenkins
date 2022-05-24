404bf3ded0a571122e57547b96cbdbb4ba5264c4
weamon <56649964+weamon@users.noreply.github.com>
2022-05-24 10:44:43 +0200

    Update holamundo.java

--- holamundo.java
+++ holamundo.java
@@ -1,5 +1,13 @@
 class HolaMundo {
   public static void main(String args[]) {
     System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
+    System.out.println("Hola Mundo!");
   }
 }
